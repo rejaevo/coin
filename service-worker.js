bfcc9dff35bdf271d2bbfa972509b5341a23d556
BuildTools <unconfigured@null.spigotmc.org>
2021-08-21 00:42:36 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e75a539e371710cd9938124c410980e8.js"
+  "./precache-manifest.5895bc0c87107e9ff25a9eca401ed0da.js"
 );
 
 self.addEventListener('message', (event) => {
